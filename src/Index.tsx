f590865bf4bf0ea1db2ae8181b8c8ea17cfb5814
Luis T. Rodrigues <luisthalys1@hotmail.com>
2023-08-04 09:44:12 -0300

    :sparkles: feat: adicionando lógica de redirecionamento

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,12 +1,46 @@
-import {Outlet} from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import {Outlet, useNavigate} from 'react-router-dom'
+import { CircularProgress } from '@mui/material';
 
 function Index() {  
 
+  const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsLoading(true);
+    }, 2500)
+
+    return () => { 
+      clearTimeout(timeout);
+    }  
+    
+  }, []);
+
+  useEffect(() => {
+    if(!isLoading) {
+      navigate('/login')
+    }
+  }, [isLoading, navigate]);
+
   return (
     <>
-      <Outlet/>
+    {isLoading ? ( 
+      <article className='bg-fundos-escuro text-texto-white w-36'>
+        <div className='m-5 pt-4 flex-col text-center'>
+          <h1 className='mb-4'>Carregando...</h1>
+          <CircularProgress className='mb-4'/>
+        </div>
+      </article>
+    ) : (
+      <>  
+        <Outlet/>
+      </>
+    )}
     </>
-  )
-}
+  );
+
+};
 
 export default Index
